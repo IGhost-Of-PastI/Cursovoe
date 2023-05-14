301dd7477178cad6f13e1ee374de46304c49414d
Timothy Timothy <tbordovich@gmail.com>
2023-05-15 02:55:33 +0300

    Переработанные функции

--- Cursovoe/Cursovoe.cpp
+++ Cursovoe/Cursovoe.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <list>
+#include <Windows.h>
 
 using namespace std;
 
@@ -9,56 +10,122 @@ struct Point
     double y;
 };
 
-bool SearchAndInput(list<Point>& points,Point newPoint)
+void OutPutPoints(list<Point>& points)
 {
-    bool result=false;
     for (auto bIter = points.begin(); bIter != points.end(); bIter++)
     {
-        if ((*bIter).x > newPoint.x)
+        cout << "Точка " << "x: " << (*bIter).x << " y: " << (*bIter).y << endl;
+    }
+}
+
+void SearchAndInput(list<Point>& points,Point newPoint)
+{
+    bool result=false;
+    //*** Проверка не существует ли точки с такой же координатой x в списке   
+    bool IsExist=false;
+    for (auto bIter = points.begin(); bIter != points.end(); bIter++)
+    {
+        if ((*bIter).x == newPoint.x)
         {
-            points.insert(--bIter, newPoint);
-            result = true;
+            IsExist = true;
+            cout << "Точка с такой координатой x существует";
             break;
         }
     }
-    if(result != true)
+    //***
+    //*** Если такой точки не существут то доабвить точку
+    if (IsExist==false)
     {
-
+        //*** Чтобы проще искать сосдение точки, список заполняется в определённом порядке
+        for (auto bIter = points.begin(); bIter != points.end(); bIter++)
+        {
+            //*** Поиск точки чья координата x больше координаты новой точки
+            if ((*bIter).x > newPoint.x)
+            {
+                //*** Если такая точка найдена то передней вставляется новая точка
+                if (bIter == points.begin())
+                {
+                    points.push_front(newPoint);
+                    result = true;
+                    break;
+                }
+                else
+                {
+                    points.insert(--bIter, newPoint);
+                    result = true;
+                    break;
+                }
+                //***
+            }
+            //***
+        }
+        //*** Если такая точка не найдена то просто добавить новую точку в конец списка.
+        if (result != true)
+        {
+            points.push_back(newPoint);
+        }
+        //***
     }
+    //***
 }
 
 void DataInput(list<Point>& points)
 {
-    for (;;)
+    int count;
+    cout << "Введите колличество точке, которыми вы зададите функцию" << endl;
+    cin >> count;
+
+    for (int i=0;i<count;i++)
     {
+        //*** Ввод новой точки
         Point temp;
         cout << "Введите X:" << endl;
         cin >> temp.x;
         cout << "Введите Y:" << endl;
         cin >> temp.y;
+        //***
 
-        if(points.empty() == true)
+        //*** Попытка добавить точку в список
+        SearchAndInput(points,temp);
+        //***
+    }
+    OutPutPoints(points);
+}
+
+void LinearInterpolation(list<Point>& points) {
+    int count;
+    cout << "Введите колличество аргументов функции(x), для получения при помощи интепроляции, соответствующих им значений" << endl;
+    cin >> count;
+    
+    for (int i=0;i<count;i++)
+    {
+        double interpolX;
+        cout << "Введите значение x точки, чтобы получить приближенное значение y при помощи линейной интерполяции. В пределах "<< (*points.begin()).x << " и "<< (*--points.end()).x << endl;
+        cin >> interpolX;
+
+        if ((*points.begin()).x < interpolX && interpolX < (*--points.end()).x)
         {
-            points.push_front(temp);
+            for (auto bIter = points.begin(); bIter != points.end(); bIter++)
+            {
+                if ((*bIter).x > interpolX)
+                {
+                   cout<< "y: " << (*(prev(bIter))).y + ((*bIter).y - (*(prev(bIter))).y) * ((interpolX - (*(prev(bIter))).x) / ((*bIter).x - (*(prev(bIter))).x)) << endl;
+                    break;
+                }
+
+            }
         }
         else
         {
-           
+            cout << "Введенный x за пределами зоны определения функции"<<endl;
         }
     }
 }
 
-double linear_interpolation(double x1, double y1, double x2, double y2, double x) {
-    return y1 + (y2 - y1) * ((x - x1) / (x2 - x1));
-}
-
 int main() {
+    setlocale(LC_ALL, "Rus");
     list<Point> points;
-
-    double x[] = { 0, 1, 2, 3, 4 };
-    double y[] = { 0, 1, 4, 9, 16 };
-    double x_new = 2.5;
-    double y_new = linear_interpolation(x[2], y[2], x[3], y[3], x_new);
-    cout << "The value of f(" << x_new << ") is approximately " << y_new << endl;
+    DataInput(points);
+    LinearInterpolation(points);
     return 0;
 }
